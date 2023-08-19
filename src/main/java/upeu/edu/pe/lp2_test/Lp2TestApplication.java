b7eb208ce1fed8b2933130b55c8e398edae0a8f6
diego <diego@Gratt>
2023-08-19 12:31:16 -0500

    Creando Packages

---
+++ src/main/java/upeu/edu/pe/lp2_test/Lp2TestApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.lp2_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/upeu/edu/pe/lp2_test/Lp2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.lp2_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
